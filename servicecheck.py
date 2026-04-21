0337adc08d4c84dda86ca9cae0df640fdc0788c5
Kuntal1985 <kuntal1985@gmail.com>
2026-04-21 11:36:46 +0530

    Update servicecheck.py

--- servicecheck.py
+++ servicecheck.py
@@ -5,10 +5,10 @@ SERVICE = "nginx"
 def check_service():
     status = os.system(f"systemctl is-active --quiet {SERVICE}")
     if status != 0:
-        print(f"❌ {SERVICE} is down. Restarting...")
+        print(f" {SERVICE} is down. Restarting...")
         os.system(f"systemctl restart {SERVICE}")
-        print(f"✅ {SERVICE} restarted")
+        print(f" {SERVICE} restarted")
     else:
-        print(f"✅ {SERVICE} is running")
+        print(f" {SERVICE} is running")
 
 check_service()
